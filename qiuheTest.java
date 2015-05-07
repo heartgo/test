f1f755665402e3cff1e8f2040fb8be0055b03f22
heartgo <237485018@qq.com>
2015-05-07 22:14:16 +0800

    test

---
+++ qiuheTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class qiuheTest {
+	qiuhe qiuhe = new qiuhe();
+
+	@Test
+	public void testSum() {
+		  assertEquals(5050, qiuhe.sum());
+	}
+
+}
